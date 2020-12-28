2e3143d5e0bd92b5ea573930db956eb1f0b7f577
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-28 18:26:16 +0200

    Improved a "Pig-Latin" solution
    Added a regex

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,43 +1,14 @@
 object PigLatin {
 
-    private val vowels = listOf("a", "e", "i", "o", "u", "xr", "yt")
-    private const val ruleThreeConsonant = "qu"
-    private const val suffix = "ay"
 
-    private val consonantClusters = listOf("ch", ruleThreeConsonant, "thr", "th", "sch", "rh")
-    fun translate(phrase: String): String = buildString {
-        for (word in phrase.split(" ")) {
-            append(convert(word))
-            append(" ")
-        }
-    }.trim()
-
-    private fun convert(word: String): String {
-        val vowel = vowels.find { word.startsWith(it) }
-        return if (vowel != null) {
-            word + suffix
-        } else {
-            processConsonantStart(word)
-        }
-    }
-
-    private fun processConsonantStart(word: String): String = buildString {
-        val consonant = StringBuilder()
-        val consonantCluster = consonantClusters.find { word.startsWith(it) }
-        if (consonantCluster != null) {
-            consonant.append(consonantCluster)
-        } else {
-            consonant.append(word[0])
-        }
-        val consonantlessWord = StringBuilder(word.substring(consonant.length))
-        if (consonantlessWord.startsWith(ruleThreeConsonant)) {
-            consonantlessWord.delete(0, 2)
-            consonant.append(ruleThreeConsonant)
-        }
-
-        append(consonantlessWord)
-        append(consonant)
-        append(suffix)
-
-    }
+    fun translate(phrase: String): String =
+        Regex("""
+            (?:
+                (?<vowel>[aeiou]|xr|yt)                 # vowels
+                    |
+                (?<consonant>ch|rh|\w?qu|thr?|sch|\w)   # consonants
+            )                                           # a non-capturing group to capture either vowels or consonants
+            (?<body>\w+)                                # unmodifiable part of the word 
+            """.trimIndent(), RegexOption.COMMENTS)
+            .replace(phrase, "\${vowel}\${body}\${consonant}ay")
 }
