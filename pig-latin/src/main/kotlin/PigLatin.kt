6ea45d8a870f212b0f261d0f153f91d70dd8133c
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-28 17:44:31 +0200

    Draft of a "Pig-Latin" solution

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,6 +1,32 @@
 object PigLatin {
 
-    fun translate(phrase: String): String {
-        TODO("Implement this function to complete the task")
+    private val vowels = listOf("a", "e", "i", "o", "u", "xr", "yt")
+    private val consonantClusters = listOf("ch", "qu", "squ", "thr", "th", "sch", "rh")
+
+    fun translate(phrase: String): String = buildString {
+        for (word in phrase.split(" ")) {
+            append(convert(word))
+            append(" ")
+        }
+    }.trim()
+
+
+    private fun convert(phrase: String): String {
+        val vowel = vowels.find { phrase.startsWith(it) }
+        return if (vowel != null) {
+            phrase + "ay"
+        } else {
+            processConsonantStart(phrase)
+        }
+    }
+
+    // todo: make it use rules 3 & 4
+    private fun processConsonantStart(phrase: String): String {
+        val consonantCluster = consonantClusters.find { phrase.startsWith(it) }
+        return if (consonantCluster != null) {
+            phrase.substring(consonantCluster.length) + consonantCluster + "ay"
+        } else {
+            phrase.substring(1) + phrase[0] + "ay"
+        }
     }
 }
