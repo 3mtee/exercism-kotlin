4839e01f4168500add373eac2c90b6246e1d4fd0
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-29 21:17:28 +0200

    Implemented the "beer song" solution

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -1,5 +1,32 @@
 object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int){
-        TODO("Implement the function to complete the task")
+    fun verses(startBottles: Int, takeDown: Int) = buildString {
+        var bottlesLeft = startBottles
+        do {
+            append(makeBottlesPhrase(bottlesLeft, true))
+                .append(" of beer on the wall, ${makeBottlesPhrase(bottlesLeft)} of beer.")
+                .append("\n")
+
+            val subtractee = if (--bottlesLeft == 0) "it" else "one"
+            if (bottlesLeft >= 0) {
+                append("Take $subtractee down and pass it around, ")
+                    .append(makeBottlesPhrase(bottlesLeft))
+                    .append(" of beer on the wall.\n")
+            } else {
+                append("Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+            }
+            if (bottlesLeft >= takeDown) {
+                append("\n")
+            }
+        } while (bottlesLeft >= takeDown)
+
+    }
+
+    private fun makeBottlesPhrase(count: Int, capitalize: Boolean = false): String {
+        val phrase = when {
+            count == 1 -> "1 bottle"
+            count <= 0 -> "no more bottles"
+            else -> "$count bottles"
+        }
+        return if (capitalize) phrase.capitalize() else phrase
     }
 }
