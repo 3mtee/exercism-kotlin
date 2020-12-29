98e62f9bcaf8eb8461468b1962efc4c8c2d825c1
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-29 13:17:52 +0200

    Implemented the "Diamond" exercise solution

--- diamond/src/main/kotlin/Diamond.kt
+++ diamond/src/main/kotlin/Diamond.kt
@@ -1,5 +1,38 @@
 class DiamondPrinter {
 
+    private val alphabet = CharArray(26) { (it + 65).toChar() }
 
+    fun printToList(letter: Char): List<String> {
+        val result: MutableList<String> = mutableListOf()
+        val index = alphabet.indexOf(letter)
 
+        for (i in 0..index) {
+            result.add(buildLine(index, i))
+        }
+        for (i in index - 1 downTo 0) {
+            result.add(buildLine(index, i))
+        }
+        return result
+
+    }
+
+    private fun buildLine(letterIndex: Int, lineIndex: Int) = buildString {
+        val spaceChar = " "
+        for (j in lineIndex until letterIndex) {
+            append(spaceChar)
+        }
+        val letter = alphabet[lineIndex]
+        append(letter)
+
+        if (lineIndex != 0 && lineIndex != (letterIndex + 1) * 2 - 1) {
+            for (k in 0..(lineIndex - 1) * 2) {
+                append(spaceChar)
+            }
+            append(letter)
+        }
+        for (j in lineIndex until letterIndex) {
+            append(spaceChar)
+        }
+    }
 }
+
