8049fec279d8916a3ea07bd87431fe1c9a7297ac
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-29 14:41:13 +0200

    Implemented the "Bob" exercise solution

--- bob/src/main/kotlin/Bob.kt
+++ bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,18 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+
+        val trimmedInput = input.trim()
+        return when {
+            trimmedInput.contains(Regex("[a-zA-Z]")) && trimmedInput == trimmedInput.toUpperCase() -> {
+                if (trimmedInput.endsWith("?")) {
+                    "Calm down, I know what I'm doing!"
+                } else {
+                    "Whoa, chill out!"
+                }
+            }
+            trimmedInput.endsWith("?") -> "Sure."
+            input.isBlank() -> "Fine. Be that way!"
+            else -> "Whatever."
+        }
     }
 }
