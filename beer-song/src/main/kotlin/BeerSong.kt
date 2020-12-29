afe9afdc8df3730ff15c3830e967b02909574caa
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2020-12-29 21:23:15 +0200

    Changed the "Beer Song" solution

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -1,32 +1,16 @@
 object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int) = buildString {
-        var bottlesLeft = startBottles
-        do {
-            append(makeBottlesPhrase(bottlesLeft, true))
-                .append(" of beer on the wall, ${makeBottlesPhrase(bottlesLeft)} of beer.")
-                .append("\n")
-
-            val subtractee = if (--bottlesLeft == 0) "it" else "one"
-            if (bottlesLeft >= 0) {
-                append("Take $subtractee down and pass it around, ")
-                    .append(makeBottlesPhrase(bottlesLeft))
-                    .append(" of beer on the wall.\n")
-            } else {
-                append("Go to the store and buy some more, 99 bottles of beer on the wall.\n")
-            }
-            if (bottlesLeft >= takeDown) {
-                append("\n")
-            }
-        } while (bottlesLeft >= takeDown)
-
+    fun verses(start: Int, end: Int): String {
+        return (start downTo end).joinToString("\n") { verse(it) }
     }
 
-    private fun makeBottlesPhrase(count: Int, capitalize: Boolean = false): String {
-        val phrase = when {
-            count == 1 -> "1 bottle"
-            count <= 0 -> "no more bottles"
-            else -> "$count bottles"
+    private fun verse(n: Int): String {
+        return when {
+            n in 2..99 -> "$n bottles of beer on the wall, $n bottles of beer.\nTake one down and pass it around, ${n - 1} ${if (n == 2) "bottle" else "bottles"} of beer on the wall.\n"
+            n == 1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+            n == 0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+            n < 0  -> throw IllegalArgumentException("Beer song verse can't be negative")
+            n > 99 -> throw IllegalArgumentException("Beer song only goes up to verse 99")
+            else   -> ""
         }
-        return if (capitalize) phrase.capitalize() else phrase
     }
 }
