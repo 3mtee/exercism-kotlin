6099d77200914e556846bd6a87ae719f9c4b6ce5
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-12 14:20:15 +0200

    Did the "Luhn" exercise

--- luhn/src/main/kotlin/Luhn.kt
+++ luhn/src/main/kotlin/Luhn.kt
@@ -1,6 +1,29 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val numCandidate = candidate.replace(Regex(" +"), "")
+        if (numCandidate.toLongOrNull() == null ||  numCandidate.length < 2) {
+            return false
+        }
+        val result: Int = numCandidate
+            .toList()
+            .reversed()
+            .mapIndexed { index, char -> processEvenDigit(char - '0', index) }
+            .reduce { acc, unit -> acc + unit }
+
+        return result % 10 == 0
+    }
+
+    private fun processEvenDigit(char: Int, index: Int): Int {
+        return if (index % 2 == 1) {
+            val multi = char * 2
+            if (multi > 9) {
+                multi - 9
+            } else {
+                multi
+            }
+        } else {
+            char
+        }
     }
 }
