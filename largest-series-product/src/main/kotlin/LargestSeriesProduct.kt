88a1dd72dafabf59a8413c875bc222bdaa682134
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-17 10:57:40 +0200

    Did the "largest Series Product" exercise

--- largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
+++ largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
@@ -1,8 +1,25 @@
-class Series {
+class Series(input: String) {
 
-    // TODO: Implement proper constructor
+    private val digits: List<Int> = input.map {
+        require(it.isDigit()) { "Input contains non-digits" }
+        it - '0'
+    }
 
     fun getLargestProduct(span: Int): Long {
-        TODO("Implement this function to complete the task")
+        if (span == 0) {
+            return 1
+        }
+        require(span > 0) { "Negative span is forbidden" }
+        val count = this.digits.count()
+        require(span <= count) { "Span shouldn't exceed the size of the input" }
+
+        var result = 0L
+        for (i in 0..count - span) {
+            result = digits.subList(i, i + span)
+                .reduce { acc, d -> acc * d }
+                .toLong()
+                .takeIf { it > result } ?: result
+        }
+        return result
     }
 }
