15f22e4f02074fabb42c934edd716c5c5538209d
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-22 11:14:18 +0200

    Refined the "Scrabble Score" exercise solution

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,24 +1,14 @@
 object ScrabbleScore {
-    private val scoreMap: Map<Char, Int> = mapOf(
-        "a,e,i,o,u,l,n,r,s,t" to 1,
-        "d,g" to 2,
-        "b,c,m,p" to 3,
-        "f,h,v,w,y" to 4,
-        "k" to 5,
-        "j,x" to 8,
-        "q,z" to 10
-    )
-        .asSequence()
-//        convert a sequence of lists to a sequence of maps
-        .map { entry -> entry.key.split(',').associate { Pair(it[0], entry.value) } }
-//        flatten
-        .flatMap { it.entries }
-//        convert to map
-        .associate { Pair(it.key, it.value) }
 
-
-    private fun scoreLetter(c: Char): Int {
-        return scoreMap.getOrDefault(c.toLowerCase(), 0)
+    private fun scoreLetter(c: Char): Int = when (c.toLowerCase()) {
+        in "aeioulnrst" -> 1
+        in "dg" -> 2
+        in "bcmp" -> 3
+        in "fhvwy" -> 4
+        in "k" -> 5
+        in "jx" -> 8
+        in "qz" -> 10
+        else -> 0
     }
 
     fun scoreWord(word: String): Int {
