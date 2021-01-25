0d552e0a627dc9e80785a0d829a13bab054a5045
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-25 18:29:43 +0200

    Did the "Roman Numerals" exercise

--- roman-numerals/src/main/kotlin/RomanNumerals.kt
+++ roman-numerals/src/main/kotlin/RomanNumerals.kt
@@ -1,6 +1,36 @@
+internal enum class RomanNumeral(val value: Int) {
+    M(1000),
+    CM(900),
+    D(500),
+    CD(400),
+    C(100),
+    XC(90),
+    L(50),
+    XL(40),
+    X(10),
+    IX(9),
+    V(5),
+    IV(4),
+    I(1)
+}
+
 object RomanNumerals {
 
     fun value(n: Int): String {
-        TODO("Implement this function to complete the task")
+        var number = n
+        val romans = RomanNumeral.values()
+        val result = StringBuilder()
+
+        var i = 0
+        while (number > 0 && i < romans.size) {
+            val currentSymbol = romans[i]
+            if (currentSymbol.value <= number) {
+                result.append(currentSymbol.name)
+                number -= currentSymbol.value
+            } else {
+                i++
+            }
+        }
+        return result.toString()
     }
 }
