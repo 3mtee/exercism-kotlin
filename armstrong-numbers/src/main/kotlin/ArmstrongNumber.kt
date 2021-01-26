ee46eef4e1e447d37efa79f79c13fcf3a3c356a4
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-26 10:34:58 +0200

    Did the "Armstrong Numbers" exercise

--- armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -1,7 +1,14 @@
+import kotlin.math.pow
+
 object ArmstrongNumber {
 
     fun check(input: Int): Boolean {
-        TODO("Implement the function to complete the task")
+        val sInput = input.toString()
+        return sInput
+            .asSequence()
+            .map { (it - '0').toDouble() }
+            .fold(0.0) { acc, i -> acc + i.pow(sInput.length) }
+            .toInt() == input
     }
 
 }
