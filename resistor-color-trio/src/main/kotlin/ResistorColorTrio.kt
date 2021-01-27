e727eeeb43e661f58feef24165f51d11cd2a45b9
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-27 11:15:05 +0200

    Did the "Resistor Color Trio" exercise

--- resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
+++ resistor-color-trio/src/main/kotlin/ResistorColorTrio.kt
@@ -1,6 +1,19 @@
+import kotlin.math.log
+import kotlin.math.pow
+
 object ResistorColorTrio {
 
-    fun text(vararg input: Color): String {
-        TODO("Implement this to complete the task")
+    fun text(vararg input: Color): String = input
+        .foldIndexed("") { index, acc, color ->
+            acc + if (index < 2) color.ordinal else "0".repeat(color.ordinal)
+        }
+        .run { toInt() }
+        .run { formatResistance(this) }
+
+    private fun formatResistance(value: Int): String {
+        val unitOrdinal = log(value.toDouble(), 1000.0).toInt()
+        val unit = Unit.values()[unitOrdinal].name.toLowerCase()
+        val n = value / 1_000.0.pow(unitOrdinal).toInt()
+        return "$n $unit"
     }
 }
