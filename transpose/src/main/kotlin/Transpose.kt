51f505786458e8a8349fb87cd587bb991992c80b
3mtee <vasilenko.sergey@gmail.com>
2021-01-28 21:30:20 +0200

    Did the "Transpose" exercise

--- transpose/src/main/kotlin/Transpose.kt
+++ transpose/src/main/kotlin/Transpose.kt
@@ -1,6 +1,20 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
+        return (0 until getLongestWordLength(input))
+            .map { column ->
+                input.indices.map { row ->
+                    input[row].getOrNull(column) ?: padOutput(column, input, row)
+                }
+            }
+            .map { it.joinToString("") }
     }
+
+    private fun padOutput(
+        column: Int,
+        input: List<String>,
+        row: Int
+    ) = if (column < getLongestWordLength(input.drop(row + 1))) " " else ""
+
+    private fun getLongestWordLength(input: List<String>) = input.maxByOrNull { it.length }?.length ?: 0
 }
