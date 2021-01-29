c78b0794282f303f901be1623c4d0f1c7caab22d
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-29 21:29:07 +0200

    Did the "Space Age" exercise

--- space-age/src/main/kotlin/SpaceAge.kt
+++ space-age/src/main/kotlin/SpaceAge.kt
@@ -1,13 +1,18 @@
-class SpaceAge {
+class SpaceAge(
+     age: Int
+){
+    private val earthAge = age / 31557600.0
 
-    // TODO: Implement proper constructor
+    fun onEarth(): Double = calculateAge(1.0)
+    fun onMercury(): Double = calculateAge(0.2408467)
+    fun onVenus(): Double = calculateAge(0.61519726)
+    fun onMars(): Double = calculateAge(1.8808158)
+    fun onJupiter(): Double = calculateAge(11.862615)
+    fun onSaturn(): Double = calculateAge(29.447498)
+    fun onUranus(): Double = calculateAge(84.016846)
+    fun onNeptune(): Double = calculateAge(164.79132)
 
-    fun onEarth(): Double = TODO("Implement this function to complete the task")
-    fun onMercury(): Double = TODO("Implement this function to complete the task")
-    fun onVenus(): Double = TODO("Implement this function to complete the task")
-    fun onMars(): Double = TODO("Implement this function to complete the task")
-    fun onJupiter(): Double = TODO("Implement this function to complete the task")
-    fun onSaturn(): Double = TODO("Implement this function to complete the task")
-    fun onUranus(): Double = TODO("Implement this function to complete the task")
-    fun onNeptune(): Double = TODO("Implement this function to complete the task")
+    private fun calculateAge(planetRatio: Double) = (earthAge / planetRatio).round()
+
+    private fun Double.round(): Double = "%.2f".format(this).toDouble()
 }
