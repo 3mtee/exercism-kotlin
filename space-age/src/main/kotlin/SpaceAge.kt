84f973b93c37d156e66e409191442bc5e900feac
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-01-29 21:35:25 +0200

    Refined the "Space Age" exercise

--- space-age/src/main/kotlin/SpaceAge.kt
+++ space-age/src/main/kotlin/SpaceAge.kt
@@ -1,18 +1,35 @@
+import PlanetAge.*
+
 class SpaceAge(
-     age: Int
-){
-    private val earthAge = age / 31557600.0
+    age: Int
+) {
 
-    fun onEarth(): Double = calculateAge(1.0)
-    fun onMercury(): Double = calculateAge(0.2408467)
-    fun onVenus(): Double = calculateAge(0.61519726)
-    fun onMars(): Double = calculateAge(1.8808158)
-    fun onJupiter(): Double = calculateAge(11.862615)
-    fun onSaturn(): Double = calculateAge(29.447498)
-    fun onUranus(): Double = calculateAge(84.016846)
-    fun onNeptune(): Double = calculateAge(164.79132)
+    companion object {
+        private const val EARTH_SECONDS = 31557600.0
+    }
 
-    private fun calculateAge(planetRatio: Double) = (earthAge / planetRatio).round()
+    private val earthAge = age / Companion.EARTH_SECONDS
+    fun onEarth(): Double = calculateAge(EARTH)
+    fun onMercury(): Double = calculateAge(MERCURY)
+    fun onVenus(): Double = calculateAge(VENUS)
+    fun onMars(): Double = calculateAge(MARS)
+    fun onJupiter(): Double = calculateAge(JUPITER)
+    fun onSaturn(): Double = calculateAge(SATURN)
+    fun onUranus(): Double = calculateAge(URANUS)
+    fun onNeptune(): Double = calculateAge(NEPTUNE)
+
+    private fun calculateAge(planet: PlanetAge) = (earthAge / planet.earthRatio).round()
 
     private fun Double.round(): Double = "%.2f".format(this).toDouble()
+}
+
+enum class PlanetAge(val earthRatio: Double) {
+    EARTH(1.0),
+    MERCURY(0.2408467),
+    VENUS(0.61519726),
+    MARS(1.8808158),
+    JUPITER(11.862615),
+    SATURN(29.447498),
+    URANUS(84.016846),
+    NEPTUNE(164.79132)
 }
