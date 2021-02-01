bdce59772ec8926b2d5022a49d6e846c2c1a80bb
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-01 17:43:04 +0200

    Did the "Saddle points" exercise

--- saddle-points/src/main/kotlin/SaddlePoints.kt
+++ saddle-points/src/main/kotlin/SaddlePoints.kt
@@ -1,1 +1,20 @@
+class Matrix(private val matrix: List<List<Int>>) {
+
+    val saddlePoints: Set<MatrixCoordinate>
+        get() {
+            val result = mutableSetOf<MatrixCoordinate>()
+            matrix.forEachIndexed { i, row ->
+                row.forEachIndexed { j, v ->
+                    if (isSaddlePoint(v, row, j)) {
+                        result.add(MatrixCoordinate(i + 1, j + 1))
+                    }
+                }
+            }
+            return result
+        }
+
+    private fun isSaddlePoint(value: Int, row: List<Int>, colIndex: Int) =
+        value == row.maxOrNull() && value == matrix.map { it[colIndex] }.minOrNull()
+}
+
 data class MatrixCoordinate(val row: Int, val col: Int)
