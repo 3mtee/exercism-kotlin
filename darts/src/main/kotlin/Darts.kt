4e56832f780ee7a63950fddf8c0e43164b30775d
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-02 11:02:23 +0200

    Did the "Darts" exercise

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,14 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun <T : Number> score(x: T, y: T): Int {
+        return when (sqrt(x.toDouble().pow(2) + y.toDouble().pow(2))) {
+            in 0.0..1.0 -> 10
+            in 0.0..5.0 -> 5
+            in 0.0..10.0 -> 1
+            else -> 0
+        }
     }
 }
