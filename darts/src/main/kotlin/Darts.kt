adf42d113b30a04927abd1ca244f59ba0337d610
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-02 11:04:27 +0200

    Refined the "Darts" exercise

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,10 +1,9 @@
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.hypot
 
 object Darts {
 
-    fun <T : Number> score(x: T, y: T): Int {
-        return when (sqrt(x.toDouble().pow(2) + y.toDouble().pow(2))) {
+    fun score(x: Number, y: Number): Int {
+        return when (hypot(x.toDouble(), y.toDouble())) {
             in 0.0..1.0 -> 10
             in 0.0..5.0 -> 5
             in 0.0..10.0 -> 1
