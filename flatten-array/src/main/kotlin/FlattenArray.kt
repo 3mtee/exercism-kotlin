f0994ce882c42a0b9f8dd558d2f0abb68241ab5d
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-03 17:30:32 +0200

    Did the "Flatten Array" exercise

--- flatten-array/src/main/kotlin/FlattenArray.kt
+++ flatten-array/src/main/kotlin/FlattenArray.kt
@@ -1,5 +1,13 @@
 object Flattener {
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+        val results = mutableListOf<Any>()
+        source.forEach {
+            if (it is Collection<Any?>) {
+                results.addAll(flatten(it))
+            } else if (it != null) {
+                results.add(it)
+            }
+        }
+        return results
     }
 }
