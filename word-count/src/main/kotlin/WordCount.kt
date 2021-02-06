482296dbb5b53651aa570a384a99fe5e5d2abeba
3mtee <vasilenko.sergey@gmail.com>
2021-02-06 12:12:42 +0200

    Did the "Word Count" exercise

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,14 @@
 object WordCount {
 
-    fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
-    }
+    fun phrase(phrase: String): Map<String, Int> = phrase
+        .split(Regex("[\\s,.]"))
+        .filter { it.isNotEmpty() }
+        .map { clearWord(it) }
+        .groupingBy { it }
+        .eachCount()
+
+    private fun clearWord(word: String) = word
+        .trim('\'') // remove any quotations around words
+        .replace(Regex("[^\\w\\d']"), "") // leave only alhanums + quotes
+        .toLowerCase()
 }
