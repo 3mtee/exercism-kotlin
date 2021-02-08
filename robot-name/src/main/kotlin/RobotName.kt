5024300d7ddf117ce3e47d7f8c983cf92e2a94a0
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-08 11:54:09 +0200

    Did the "Robot Name" exercise

--- robot-name/src/main/kotlin/RobotName.kt
+++ robot-name/src/main/kotlin/RobotName.kt
@@ -1,9 +1,35 @@
+import kotlin.random.Random
+
 class Robot {
 
-    val name: String
-        get() = TODO("Implement this getter to complete the task")
+    companion object {
+        @JvmStatic
+        private val CHAR_POOL = ('A'..'Z')
+
+        @JvmStatic
+        private val NUM_POOL = ('0'..'9')
+
+        private val NAMES = mutableSetOf<String>()
+    }
+
+    var name: String = generateName()
+
+    private fun generateName(): String {
+        var newName: String
+        do {
+            newName = CHAR_POOL.randomChars(2) + NUM_POOL.randomChars(3)
+        } while (newName in NAMES)
+
+        NAMES.add(newName)
+        return newName
+    }
 
     fun reset() {
-        TODO("Implement this function to complete the task")
+        name = generateName()
     }
 }
+
+private fun CharRange.randomChars(count: Int) = (1..count)
+    .map { elementAt(Random.nextInt(count())) }
+    .joinToString("")
+
