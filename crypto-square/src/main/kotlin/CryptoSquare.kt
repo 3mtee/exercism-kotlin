e33e29ee573b1ed67e69e575d792c653956742d1
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-09 14:11:10 +0200

    Refined the "Crypto Square" exercise

--- crypto-square/src/main/kotlin/CryptoSquare.kt
+++ crypto-square/src/main/kotlin/CryptoSquare.kt
@@ -4,33 +4,21 @@ import kotlin.math.sqrt
 object CryptoSquare {
 
     fun ciphertext(plaintext: String): String {
-        if (plaintext.isBlank()) {
-            return ""
-        }
-        val normalizedText = plaintext
-            .replace(Regex("[^\\w\\d]"), "")
+
+        return plaintext
+            .filter { it.isLetterOrDigit() }
             .toLowerCase()
-
-        val length = normalizedText.length.toDouble()
-
-        val col = ceil(sqrt(length)).toInt()
-        val row = ceil(length / col).toInt()
-        val matrix = normalizedText
-            .padEnd(col * row, ' ')
-            .chunked(col)
-
-        return transpose(col, matrix)
-            .joinToString(" ") { it.joinToString("") }
+            .apply { if (isEmpty()) return "" }
+            .let {
+                val numCols = ceil(sqrt(it.length.toDouble())).toInt()
+                transpose(numCols, it.chunked(numCols))
+            }
     }
 
-    private fun transpose(
-        col: Int,
-        matrix: List<String>
-    ) = (0 until col)
-        .map { cIndex ->
-            matrix.indices.map { rIndex ->
-                matrix[rIndex].getOrNull(cIndex) ?: ' '
-            }
+    private fun transpose(numCols: Int, sq: List<String>) = (0 until numCols)
+        .joinToString(" ") { i ->
+            sq
+                .map { it.getOrNull(i) ?: ' ' }
+                .joinToString("")
         }
-
 }
