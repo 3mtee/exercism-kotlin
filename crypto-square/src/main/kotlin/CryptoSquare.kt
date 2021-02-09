901faabf068d7673dd77ed8484ff66a86875c449
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-09 13:41:32 +0200

    Did the "Crypto Square" exercise

--- crypto-square/src/main/kotlin/CryptoSquare.kt
+++ crypto-square/src/main/kotlin/CryptoSquare.kt
@@ -1,7 +1,36 @@
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
 object CryptoSquare {
 
     fun ciphertext(plaintext: String): String {
-        TODO("Implement the function to complete the task")
+        if (plaintext.isBlank()) {
+            return ""
+        }
+        val normalizedText = plaintext
+            .replace(Regex("[^\\w\\d]"), "")
+            .toLowerCase()
+
+        val length = normalizedText.length.toDouble()
+
+        val col = ceil(sqrt(length)).toInt()
+        val row = ceil(length / col).toInt()
+        val matrix = normalizedText
+            .padEnd(col * row, ' ')
+            .chunked(col)
+
+        return transpose(col, matrix)
+            .joinToString(" ") { it.joinToString("") }
     }
+
+    private fun transpose(
+        col: Int,
+        matrix: List<String>
+    ) = (0 until col)
+        .map { cIndex ->
+            matrix.indices.map { rIndex ->
+                matrix[rIndex].getOrNull(cIndex) ?: ' '
+            }
+        }
 
 }
