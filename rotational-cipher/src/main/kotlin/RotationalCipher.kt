c0cee87d262716bb3b6233deef5034326509045e
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-25 17:37:38 +0200

    Refined the "Rotational Cipher" solution

--- rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,25 +1,30 @@
 class RotationalCipher(shift: Int) {
-    private val shift = shift % 26
+    companion object{
+        @JvmStatic
+        private val ALPHABET_SIZE = ('a'..'z').count()
+    }
+
+    private val shift = shift % ALPHABET_SIZE
 
     fun encode(text: String): String {
         return text
             .asSequence()
-            .map {
-                val lowerChar = it.toLowerCase()
-                val c = lowerChar + shift > 'z'
-                val result: Char = if (c) {
-                    lowerChar - 26 + shift
-                } else if (!it.isLetter()) {
-                    lowerChar
-                } else {
-                    it + shift
-                }
-                if (!it.isLowerCase()) {
-                    return@map result.toUpperCase()
-                } else {
-                    return@map result
-                }
-            }
+            .map { rotateChar(it) }
             .joinToString("")
+    }
+
+    private fun rotateChar(char: Char): Char {
+        val lowerChar = char.toLowerCase()
+        val shiftedChar = lowerChar + shift
+        val rotationResult = when {
+            shiftedChar > 'z' -> shiftedChar - ALPHABET_SIZE
+            char.isLetter() -> shiftedChar
+            else -> lowerChar
+        }
+        return if (char.isUpperCase()) {
+            rotationResult.toUpperCase()
+        } else {
+            rotationResult
+        }
     }
 }
