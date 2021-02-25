8b17e81febe14012d1ca52371c6b69fea8b7b55c
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-02-25 17:30:47 +0200

    Did the "Rotational Cipher" exercise

--- rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,8 +1,25 @@
-class RotationalCipher {
-
-    // TODO: Implement proper constructor
+class RotationalCipher(shift: Int) {
+    private val shift = shift % 26
 
     fun encode(text: String): String {
-        TODO("Implement this function to complete the task")
+        return text
+            .asSequence()
+            .map {
+                val lowerChar = it.toLowerCase()
+                val c = lowerChar + shift > 'z'
+                val result: Char = if (c) {
+                    lowerChar - 26 + shift
+                } else if (!it.isLetter()) {
+                    lowerChar
+                } else {
+                    it + shift
+                }
+                if (!it.isLowerCase()) {
+                    return@map result.toUpperCase()
+                } else {
+                    return@map result
+                }
+            }
+            .joinToString("")
     }
 }
