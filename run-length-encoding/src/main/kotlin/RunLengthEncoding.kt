eb5bd73d88a8c5db6252b21cff804cdb4c679982
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-03-02 17:52:27 +0200

    Refined the "Run Length Encoding" exercise

--- run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,26 +1,12 @@
 object RunLengthEncoding {
 
-    fun encode(input: String): String {
-        return input
-            .asSequence()
-            .zipWithNext()
-            .foldIndexed(mutableListOf<MutableList<Char>>()) { index, acc, data ->
-                if (acc.isEmpty()) {
-                    acc.add(mutableListOf())
-                }
-                acc.last().add(data.first)
-                if (data.first != data.second) {
-                    acc.add(mutableListOf())
-                }
-                if (index == input.length - 2) {
-                    acc.last().add(data.second)
-                }
-                acc
-            }
-            .joinToString("") { "${if (it.size > 1) it.size else ""}${it.first()}" }
-    }
+    fun encode(input: String): String = input
+        .replace(Regex("(.)\\1+")) {
+            "${it.value.length}${it.groupValues[1]}"
+        }
 
-    fun decode(input: String): String = input.replace(Regex("(\\d+)(.)")) {
-        it.groupValues[2].repeat(it.groupValues[1].toInt())
-    }
+    fun decode(input: String): String = input
+        .replace(Regex("(\\d+)(.)")) {
+            it.groupValues[2].repeat(it.groupValues[1].toInt())
+        }
 }
