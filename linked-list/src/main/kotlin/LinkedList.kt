12ceaa4911a5949f21fcc86861959f9366e5f5e3
3mtee <vasilenko.sergey@gmail.com>
2021-03-05 21:01:27 +0200

    Did the "Linked List" exercise

--- linked-list/src/main/kotlin/LinkedList.kt
+++ linked-list/src/main/kotlin/LinkedList.kt
@@ -1,18 +1,57 @@
 class Deque<T> {
 
+    inner class Node<T>(internal val value: T) {
+        internal var previous: Node<T>? = null
+        internal var next: Node<T>? = null
+    }
+
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+
     fun push(value: T) {
-        TODO("Implement this function to complete the task")
+        val node = Node(value)
+        if (!initIfNeeded(value)) {
+            tail!!.next = node
+            node.previous = head
+            tail = node
+        }
     }
 
     fun pop(): T? {
-        TODO("Implement this function to complete the task")
+        val result = tail!!.value
+        if (tail != head) {
+            tail = tail!!.previous
+            tail!!.next = null
+        }
+        return result
     }
 
     fun unshift(value: T) {
-        TODO("Implement this function to complete the task")
+        val node = Node(value)
+        if (!initIfNeeded(value)) {
+            head!!.previous = node
+            node.next = head
+            head = node
+        }
+
     }
 
     fun shift(): T? {
-        TODO("Implement this function to complete the task")
+        val result = head!!.value
+        if (tail != head) {
+            head = head!!.next
+            head!!.previous = null
+        }
+        return result
+    }
+
+    private fun initIfNeeded(value: T): Boolean {
+        val node = Node(value)
+        if (head == null || tail == null) {
+            head = node
+            tail = node
+            return true
+        }
+        return false
     }
 }
