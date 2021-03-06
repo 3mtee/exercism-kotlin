46d55adbda9986f74db32f6bc8ab7ef6abc3a197
3mtee <vasilenko.sergey@gmail.com>
2021-03-07 00:27:58 +0200

    Did the "Perfect Numbers" exercise

--- perfect-numbers/src/main/kotlin/PerfectNumbers.kt
+++ perfect-numbers/src/main/kotlin/PerfectNumbers.kt
@@ -1,8 +1,18 @@
-
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
-fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
-}
+fun classify(naturalNumber: Int) =
+    if (naturalNumber < 1) {
+        throw IllegalArgumentException("Incorrect input!")
+    } else {
+        (1 until naturalNumber)
+            .sumBy { if (naturalNumber % it == 0) it else 0 }
+            .let {
+                when {
+                    it == naturalNumber -> Classification.PERFECT
+                    it > naturalNumber -> Classification.ABUNDANT
+                    else -> Classification.DEFICIENT
+                }
+            }
+    }
