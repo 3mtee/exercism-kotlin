f6362932891f5bf1dedd705f230f35f3e6c0aed4
3mtee <vasilenko.sergey@gmail.com>
2021-03-07 00:08:24 +0200

    Did the "Scale Generator" exercise

--- scale-generator/src/main/kotlin/Scale.kt
+++ scale-generator/src/main/kotlin/Scale.kt
@@ -1,11 +1,17 @@
 class Scale(private val tonic: String) {
 
-    fun chromatic(): List<String> {
-        TODO("Implement this function to complete the task")
+    fun chromatic(): List<String> = if (tonic.getOrNull(1) == '#') {
+        listOf("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    } else {
+        listOf("F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E")
     }
+        .run { cycleNotes() }
 
-    fun interval(intervals: String): List<String> {
-        TODO("Implement this function to complete the task")
-    }
+    private fun List<String>.cycleNotes() =
+        dropWhile { it.toUpperCase() != tonic.toUpperCase() } + takeWhile { it.toUpperCase() != tonic.toUpperCase() }
 
+    fun interval(intervals: String): List<String> =
+        intervals
+            .scan(0) { x, y -> x + "mMA".indexOf(y) + 1 }
+            .mapNotNull { chromatic().getOrNull(it) }
 }
