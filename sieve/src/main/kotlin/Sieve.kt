e7059f08291d3d07c470c50536e20e94cac6dace
3mtee <vasilenko.sergey@gmail.com>
2021-03-07 11:12:08 +0200

    Did the "Sieve" exercise

--- sieve/src/main/kotlin/Sieve.kt
+++ sieve/src/main/kotlin/Sieve.kt
@@ -1,6 +1,16 @@
 object Sieve {
 
     fun primesUpTo(upperBound: Int): List<Int> {
-        TODO("Implement this function to complete the task")
+        val composites = mutableSetOf<Int>()
+        val results = mutableListOf<Int>()
+        (2..upperBound)
+            .forEach { i ->
+                if (i !in composites) {
+                    results.add(i)
+                }
+                (i..upperBound)
+                    .forEach { j -> if (j % i ==0) composites.add(j) }
+            }
+        return results
     }
 }
