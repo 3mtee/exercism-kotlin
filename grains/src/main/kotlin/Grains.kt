461963a148def4a6187d4120d3590f5d277c5edd
3mtee <vasilenko.sergey@gmail.com>
2021-03-09 23:48:33 +0200

    Did the "Grains" exercise

--- grains/src/main/kotlin/Grains.kt
+++ grains/src/main/kotlin/Grains.kt
@@ -2,11 +2,12 @@ import java.math.BigInteger
 
 object Board {
 
-    fun getGrainCountForSquare(number: Int): BigInteger {
-        TODO("Implement this function to complete the task")
-    }
+    fun getGrainCountForSquare(number: Int): BigInteger = BigInteger.TWO
+        .apply { require(number in 1..64) { throw IllegalArgumentException("Incorrect input") } }
+        .pow(number - 1)
 
-    fun getTotalGrainCount(): BigInteger {
-        TODO("Implement this function to complete the task")
-    }
+    fun getTotalGrainCount(): BigInteger =
+        (0 until 64)
+            .map { BigInteger.TWO.pow(it) }
+            .fold(BigInteger.ZERO) { acc, count -> acc.plus(count) }
 }
