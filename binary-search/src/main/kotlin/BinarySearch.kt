036fb12d37cc95494bd8f4da574ef5185e521a4e
3mtee <vasilenko.sergey@gmail.com>
2021-03-09 10:21:06 +0200

    Did the "Binary Search" exercise

--- binary-search/src/main/kotlin/BinarySearch.kt
+++ binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,19 @@
 object BinarySearch {
     fun search(list: List<Int>, item: Int): Int {
-        TODO("Implement the function to complete the task. Change the signature if necessary.")
+        var from = 0
+        var to = list.size - 1
+
+        while (from <= to) {
+            val mid = (from + to) / 2
+            val midVal = list[mid]
+            val cResult = compareValues(midVal, item)
+
+            when {
+                cResult < 0 -> from = mid + 1
+                cResult > 0 -> to = mid - 1
+                else -> return mid
+            }
+        }
+        throw NoSuchElementException("Item cannot be not found")
     }
 }
