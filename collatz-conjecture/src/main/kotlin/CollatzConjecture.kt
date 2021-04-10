9ccf9e582af34a3deeb13b01e9ed76aa7a623c7e
3mtee <vasilenko.sergey@gmail.com>
2021-04-10 23:01:11 +0300

    Did the "Collatz Conjecture" exercise

--- collatz-conjecture/src/main/kotlin/CollatzConjecture.kt
+++ collatz-conjecture/src/main/kotlin/CollatzConjecture.kt
@@ -1,5 +1,8 @@
 object CollatzCalculator {
-    fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+    fun computeStepCount(start: Int, step: Int = 0): Int = when {
+        start < 1 -> throw IllegalArgumentException("Incorrect input")
+        start == 1 -> step
+        start % 2 == 0 -> computeStepCount(start / 2, step + 1)
+        else -> computeStepCount(start * 3 + 1, step + 1)
     }
 }
