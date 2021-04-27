b8cd6f9aff0e36cafa532b1c0a04454798a8a948
Sergey Vasilenko <vasilenko.sergey@gmail.com>
2021-04-27 11:42:41 +0300

    Did the "Prime Factors" exercise

--- prime-factors/src/main/kotlin/PrimeFactors.kt
+++ prime-factors/src/main/kotlin/PrimeFactors.kt
@@ -1,10 +1,19 @@
 object PrimeFactorCalculator {
 
-    fun primeFactors(int: Int): List<Int> {
-        TODO("Implement this function to complete the task")
-    }
+    fun primeFactors(int: Int): List<Int> = primeFactors(int.toLong()).map { it.toInt() }
 
     fun primeFactors(long: Long): List<Long> {
-        TODO("Implement this function to complete the task")
+        val results = mutableListOf<Long>()
+        var num = long
+        var divider = 2L
+        while (num > 1) {
+            if (num % divider == 0L) {
+                num /= divider
+                results.add(divider)
+            } else {
+                divider++
+            }
+        }
+        return results
     }
 }
