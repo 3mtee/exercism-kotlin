a516dd11ef82214120ca350ecafdc2728edcc582
3mtee <vasilenko.sergey@gmail.com>
2021-04-29 23:25:36 +0300

    Did the "Kindergarten Garden" exercise

--- kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -1,6 +1,31 @@
 class KindergartenGarden(private val diagram: String) {
+    companion object {
+        @JvmStatic
+        private val students = listOf(
+            "Alice",
+            "Bob",
+            "Charlie",
+            "David",
+            "Eve",
+            "Fred",
+            "Ginny",
+            "Harriet",
+            "Ileana",
+            "Joseph",
+            "Kincaid",
+            "Larry"
+        )
 
-    fun getPlantsOfStudent(student: String): List<String> {
-        TODO("Implement the function to complete the task")
+        @JvmStatic
+        private val plants = mapOf(
+            'G' to "grass",
+            'C' to "clover",
+            'R' to "radishes",
+            'V' to "violets"
+        )
     }
+
+    fun getPlantsOfStudent(student: String) =
+        diagram.split("\n").map { it.chunked(2)[students.indexOf(student)] }.flatMap { it.toList() }
+            .map { plants[it]!! }
 }
