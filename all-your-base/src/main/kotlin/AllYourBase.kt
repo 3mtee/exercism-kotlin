e374a8933bc35d24fa62406f876da450c9c4a91a
3mtee <vasilenko.sergey@gmail.com>
2021-04-30 23:21:48 +0300

    Did the "All Your Base" exercise

--- all-your-base/src/main/kotlin/AllYourBase.kt
+++ all-your-base/src/main/kotlin/AllYourBase.kt
@@ -1,7 +1,20 @@
-class BaseConverter {
-    // TODO: implement proper constructor to complete the task
+class BaseConverter(private val base: Int, digits: IntArray) {
+    init {
+        require(base > 1) { "Bases must be at least 2." }
+        require(digits.isNotEmpty()) { "You must supply at least one digit." }
+        require(digits[0] != 0 || digits.size == 1) { "Digits may not contain leading zeros." }
+        require(digits.all { it >= 0 }) { "Digits may not be negative." }
+        require(digits.all { it < base }) { "All digits must be strictly less than the base." }
+    }
 
-    fun convertToBase(newBase: Int): IntArray {
-        TODO("Implement the function to complete the task")
+    private val decValue = digits.fold(0) { acc, i -> acc * base + i }
+
+    fun convertToBase(newBase: Int, number: Int = decValue): IntArray {
+        require(newBase > 1) { "Bases must be at least 2." }
+        return if (number < newBase) {
+            intArrayOf(number)
+        } else {
+            convertToBase(newBase, number / newBase) + number % newBase
+        }
     }
 }
