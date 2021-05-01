3fcd77c5b7e8116420ebcee2a6f5544ad2413af3
3mtee <vasilenko.sergey@gmail.com>
2021-05-02 00:18:55 +0300

    Did the "Spiral Matrix" exercise

--- spiral-matrix/src/main/kotlin/SpiralMatrix.kt
+++ spiral-matrix/src/main/kotlin/SpiralMatrix.kt
@@ -1,6 +1,23 @@
 object SpiralMatrix {
 
     fun ofSize(size: Int): Array<IntArray> {
-        TODO("Implement this function to complete the task")
+        val matrix = Array(size) { IntArray(size) }
+
+        var startRow = 0
+        var endRow = size - 1
+        var startCol = 0
+        var endCol = size - 1
+        var number = 1
+        while (number <= size * size) {
+            for (i in startCol..endCol) matrix[startRow][i] = number++
+            for (i in ++startRow..endRow) matrix[i][endCol] = number++
+            for (i in --endCol downTo startCol) matrix[endRow][i] = number++
+            for (i in --endRow downTo startRow) matrix[i][startCol] = number++
+
+            startCol++
+        }
+
+        return matrix
+
     }
 }
