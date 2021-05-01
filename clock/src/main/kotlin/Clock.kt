dea5f0ccf3535882400b5cee1b3ec593bad7f169
3mtee <vasilenko.sergey@gmail.com>
2021-05-01 22:25:27 +0300

    Did the "Clock" exercise

--- clock/src/main/kotlin/Clock.kt
+++ clock/src/main/kotlin/Clock.kt
@@ -1,13 +1,27 @@
-class Clock {
-    //TODO: implement proper constructor
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Calendar.*
 
-    //TODO: find a convenient way to take over `toString()` and `equals()`
+class Clock(hour: Int, minute: Int) {
 
-    fun subtract(minutes: Int) {
-        TODO("Implement the function to complete the task")
+    var time: Calendar = getInstance()
+
+    init {
+        time.set(HOUR_OF_DAY, hour)
+        time.set(MINUTE, minute)
     }
 
-    fun add(minutes: Int) {
-        TODO("Implement the function to complete the task")
+    fun subtract(minutes: Int) = time.add(MINUTE, -minutes)
+
+    fun add(minutes: Int) = time.add(MINUTE, minutes)
+
+    override fun toString(): String = SimpleDateFormat("HH:mm").format(time.time)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Clock) return false
+        return time.get(HOUR_OF_DAY) == other.time.get(HOUR_OF_DAY) && time.get(MINUTE) == other.time.get(MINUTE)
     }
+
+    override fun hashCode() = time.hashCode()
 }
