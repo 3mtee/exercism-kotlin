e1060c0a9019dd787ed1c9b18750dc6b64ff6692
3mtee <vasilenko.sergey@gmail.com>
2021-05-02 11:11:40 +0300

    Did the "Matching Brackets" exercise

--- matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -1,6 +1,25 @@
 object MatchingBrackets {
+    private val bracketMap = mapOf(
+        ')' to '(',
+        ']' to '[',
+        '}' to '{'
+    )
 
     fun isValid(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val brackets = mutableListOf<Char>()
+        input.asIterable().forEach {
+            when (it) {
+                in "([{" -> brackets.add(it)
+                in ")]}" -> {
+                    if (brackets.isNotEmpty() && brackets.last() == bracketMap[it]) {
+                        brackets.removeLast()
+                    } else {
+                        return false
+                    }
+                }
+            }
+        }
+        return brackets.isEmpty()
     }
+
 }
