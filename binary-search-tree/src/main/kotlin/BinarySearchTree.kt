66481238b15241c81bc3a6d1a93fa21aeac95d92
3mtee <vasilenko.sergey@gmail.com>
2021-05-03 19:07:42 +0300

    Did the "Binary Search Tree" exercise

--- binary-search-tree/src/main/kotlin/BinarySearchTree.kt
+++ binary-search-tree/src/main/kotlin/BinarySearchTree.kt
@@ -1,19 +1,46 @@
 class BinarySearchTree<T : Comparable<T>> {
 
-    data class Node<T>(val data: T /* maybe more... */)
+    data class Node<T>(val data: T, var left: Node<T>? = null, var right: Node<T>? = null)
 
     var root: Node<T>? = null
 
-    fun insert(value: T) {
-        TODO("Delete this statement and write your own implementation.")
+    fun insert(value: T, node: Node<T>? = root) {
+        when {
+            root == null -> root = Node(value)
+            node!!.data < value -> {
+                if (node.right == null) {
+                    node.right = Node(value)
+                } else {
+                    insert(value, node.right)
+                }
+            }
+            node.data >= value -> {
+                if (node.left == null) {
+                    node.left = Node(value)
+                } else {
+                    insert(value, node.left)
+                }
+            }
+        }
+
     }
 
-    fun asSortedList(): List<T> {
-        TODO("Delete this statement and write your own implementation.")
+    fun asSortedList(node: Node<T>? = root): List<T> = if (node == null) {
+        emptyList()
+    } else {
+        asSortedList(node.left) + listOf(node.data) + asSortedList(node.right)
     }
 
     fun asLevelOrderList(): List<T> {
-        TODO("Delete this statement and write your own implementation.")
+        val nodes = mutableListOf(root)
+        var i = 0
+        while (i < nodes.size) {
+            if (nodes[i]?.left != null) nodes.add(nodes[i]?.left)
+            if (nodes[i]?.right != null) nodes.add(nodes[i]?.right)
+            i++
+        }
+        return nodes.filterNotNull().map { it.data }
     }
 
 }
+
