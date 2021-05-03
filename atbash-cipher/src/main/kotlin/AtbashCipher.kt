81a58df0aeb1aa6978d0a9698dbb7093ae971ea1
3mtee <vasilenko.sergey@gmail.com>
2021-05-03 21:56:34 +0300

    Did the "Atbash Cipher" exercise

--- atbash-cipher/src/main/kotlin/AtbashCipher.kt
+++ atbash-cipher/src/main/kotlin/AtbashCipher.kt
@@ -1,10 +1,20 @@
 object Atbash {
+    private val alphabet = 'a'..'z'
+    private val reversedAlphabet = alphabet.reversed()
+    private val encodeMap = alphabet.zip(reversedAlphabet).toMap()
+    private val decodeMap = reversedAlphabet.zip(alphabet).toMap()
+    fun encode(s: String) = s
+        .toLowerCase()
+        .asSequence()
+        .filter { it.isLetterOrDigit() }
+        .map { encodeMap.getOrDefault(it, it) }
+        .joinToString("")
+        .windowed(5, step = 5, partialWindows = true)
+        .joinToString(" ")
 
-    fun encode(s: String): String{
-        TODO("Implement the function to complete the task")
-    }
-
-    fun decode(s: String): String{
-        TODO("Implement the function to complete the task")
-    }
+    fun decode(s: String) = s
+        .toLowerCase()
+        .filter { it.isLetterOrDigit() }
+        .map { decodeMap.getOrDefault(it, it) }
+        .joinToString("")
 }
