b45063f40045c268cc6de7c2ceaa8fd8987a068e
3mtee <vasilenko.sergey@gmail.com>
2021-05-04 11:20:22 +0300

    Did the "Simple Cipher" exercise

--- simple-cipher/src/main/kotlin/SimpleCipher.kt
+++ simple-cipher/src/main/kotlin/SimpleCipher.kt
@@ -1,12 +1,41 @@
-data class Cipher(val todo: Nothing) {
+private val alphabet = 'a'..'z'
+private fun generateKey() = (1..100).map { alphabet.random() }.joinToString("")
 
-    // TODO: Implement proper constructor
+
+data class Cipher(val key: String = generateKey()) {
+    init {
+        require(key.isNotEmpty() && key.all { it.isLetter() && it.isLowerCase() }) { "Incorrect key" }
+    }
 
     fun encode(s: String): String {
-        TODO("Implement this function to complete the task")
+        return doStuff(s, false)
     }
 
     fun decode(s: String): String {
-        TODO("Implement this function to complete the task")
+        return doStuff(s, true)
+    }
+
+    private fun doStuff(text: String, decode: Boolean) = text.zip(chooseKey(text))
+        .map { getChar(it, decode) }
+        .joinToString("")
+
+    private fun chooseKey(text: String) = if (key.length < text.length) {
+        key.repeat(text.length / key.length + 1)
+    } else {
+        key
+    }
+        .substring(0, text.length)
+
+    private fun getChar(pair: Pair<Char, Char>, decode: Boolean): Char {
+        val index = if (decode) {
+            alphabet.indexOf(pair.first) - alphabet.indexOf(pair.second)
+        } else {
+            alphabet.indexOf(pair.first) + alphabet.indexOf(pair.second)
+        }
+            .let { it % alphabet.count() }
+            .let {
+                if (it >= 0) it else it + alphabet.count()
+            }
+        return alphabet.elementAt(index)
     }
 }
