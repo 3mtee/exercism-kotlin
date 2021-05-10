e70479eb65bb8377c415135a508e0251be886e7f
3mtee <vasilenko.sergey@gmail.com>
2021-05-10 18:29:30 +0300

    Did the "Sublist" exercise

--- sublist/src/main/kotlin/Sublist.kt
+++ sublist/src/main/kotlin/Sublist.kt
@@ -1,5 +1,12 @@
+import Relationship.*
+
 enum class Relationship {
-
     EQUAL, SUBLIST, SUPERLIST, UNEQUAL
+}
 
+fun <T> List<T>.relationshipTo(other: List<T>) = when {
+    this == other -> EQUAL
+    this.isEmpty() || other.windowed(this.size).contains(this) -> SUBLIST
+    other.isEmpty() || this.windowed(other.size).contains(other) -> SUPERLIST
+    else -> UNEQUAL
 }
