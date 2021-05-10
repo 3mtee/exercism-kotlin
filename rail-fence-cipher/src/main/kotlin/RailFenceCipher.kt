aa14b968909884975b53e7cda31a2498f7088f8f
3mtee <vasilenko.sergey@gmail.com>
2021-05-10 17:38:35 +0300

    Did the "Rail Fence Cipher" exercise

--- rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
+++ rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
@@ -1,12 +1,25 @@
-class RailFenceCipher(private val any:Any) {
+class RailFenceCipher(private val railFence: Int) {
 
-    // TODO: Implement proper constructor
+    fun getEncryptedData(input: String): String = getRailFenceSequence()
+        .take(input.length)
+        .zipRailFence(input.asSequence())
+        .joinToString("")
 
-    fun getEncryptedData(input: String): String {
-        TODO("Implement this function to complete the task")
-    }
+    fun getDecryptedData(input: String): String = getRailFenceSequence()
+        .take(input.length)
+        .zipRailFence(input.indices.asSequence())
+        .zipRailFence(input.asSequence())
+        .joinToString("")
 
-    fun getDecryptedData(input: String): String {
-        TODO("Implement this function to complete the task")
-    }
+    private fun getRailFenceSequence() = generateSequence(0) { it + 1 }
+        .map {
+            (0 until railFence - 1)
+                .plus(railFence - 1 downTo 1)[it % (2 * (railFence - 1))]
+        }
+
+    private fun <T : Comparable<T>, R> Sequence<T>.zipRailFence(other: Sequence<R>) =
+        this
+            .zip(other)
+            .sortedBy { it.first }
+            .map { it.second }
 }
